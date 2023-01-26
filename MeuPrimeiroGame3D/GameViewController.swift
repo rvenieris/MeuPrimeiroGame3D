eda2aa84746fb5ed872ccc03df3e32fa1264a83c
Ricardo Venieris <rvenieris@gmail.com>
2023-01-26 18:36:35 -0300

    Cameras e aros ok.

--- MeuPrimeiroGame3D/GameViewController.swift
+++ MeuPrimeiroGame3D/GameViewController.swift
@@ -14,12 +14,17 @@ class GameViewController: UIViewController {
         // create a new scene
     let scene = SCNScene(named: "art.scnassets/ship.scn")!
     
-      let cameraNode:SCNNode = {
+      lazy var cameraNode:SCNNode = {
               // create and add a camera to the scene
           let cameraNode = SCNNode()
           cameraNode.camera = SCNCamera()
               // place the camera
-          cameraNode.position = SCNVector3(x: 0, y: 0, z: 15)
+          cameraNode.transform = SCNMatrix4MakeRotation(Float.pi, 0, 1, 0)
+          cameraNode.position = SCNVector3(x: 0, y: 4, z: -13)
+          
+          // increase horizon distance
+          cameraNode.camera?.zFar = 2000
+
           return cameraNode
       }()
    
@@ -45,23 +50,47 @@ class GameViewController: UIViewController {
           // retrieve the ship node
       lazy var ship = scene.rootNode.childNode(withName: "ship", recursively: true)!
     
-    var torusNode:SCNNode {
-            // create a 3D torus object
-        let torusGeometry = SCNTorus(ringRadius: 3, pipeRadius: 0.3)
-        
-            // apply a metallic material to the torus
-        let material = SCNMaterial()
-        material.lightingModel = .physicallyBased
-        material.metalness.contents = 1
-        material.roughness.contents = 0.2
-        torusGeometry.materials = [material]
-        
-            // create a node to hold the torus
-        let torusNode = SCNNode(geometry: torusGeometry)
-        torusNode.name = "torus"
-            // add return the new torus
-        return torusNode
-    }
+      var torusNode:SCNNode {
+              // create a 3D torus object
+          let torusGeometry = SCNTorus(ringRadius: 7, pipeRadius: 0.75)
+  
+              // apply a metallic material to the torus
+          let material = SCNMaterial()
+          material.lightingModel = .physicallyBased
+          material.metalness.contents = 1
+          material.roughness.contents = 0.2
+          torusGeometry.materials = [material]
+  
+              // create a node to hold the torus
+          let torusNode = SCNNode(geometry: torusGeometry)
+          torusNode.name = "torus"
+          
+              // apply a rotation matrix to the torus node
+          torusNode.transform = SCNMatrix4MakeRotation(Float.pi/2, 1, 0, 0)
+          
+              // spawn action
+          let wait     = SCNAction.wait(duration: 15)
+          let kill     = SCNAction.removeFromParentNode()
+          let sequence = SCNAction.sequence([wait, kill])
+          torusNode.runAction(sequence)
+
+              // return the new torus
+          return torusNode
+      }
+
+             // A node to hold all torus
+      lazy var torusField:SCNNode = {
+          let torusField = SCNNode()
+          
+          // spawn action
+          let wait           = SCNAction.wait(duration: 1)
+          let spawn          = SCNAction.run {_ in self.addTorus() }
+          let sequence       = SCNAction.sequence([wait, spawn])
+          let repeatSequence = SCNAction.repeatForever(sequence)
+          torusField.runAction(repeatSequence)
+          
+          return torusField
+      }()
       
         // retrieve the SCNView
       lazy var scnView:SCNView = {
@@ -70,7 +99,7 @@ class GameViewController: UIViewController {
           scnView.allowsCameraControl = true
   
               // show statistics such as fps and timing information
-          scnView.showsStatistics = true
+          scnView.showsStatistics = false
   
               // configure the view
           scnView.backgroundColor = UIColor.black
@@ -83,52 +112,73 @@ class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        sleep(2)
         scene.rootNode.addChildNode(cameraNode)
         scene.rootNode.addChildNode(lightNode)
         scene.rootNode.addChildNode(ambientLightNode)
-        scene.rootNode.addChildNode(torusNode)
+        scene.rootNode.addChildNode(torusField)
 
         // add a tap gesture recognizer
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
         scnView.addGestureRecognizer(tapGesture)
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+    }
+    
+      func addTorus() {
+          // get last torus position
+          let lastTorusposition = torusField.childNodes.last?.position ?? SCNVector3(x: 0, y: 0, z: 0)
+          // create a new Torus
+          let newTorus = torusNode
+  
+          // generate a displacement distance generated in a "grid way" for left or right
+          let randomLR:Float = Float(([-1, 1].randomElement()!) * Int.random(in: 2...5) * 10)
+          newTorus.position.x = lastTorusposition.x + randomLR
+  
+          // generate a displacement distance random generated in a range
+          newTorus.position.z = lastTorusposition.z + Float.random(in:  50...80)
+  
+          torusField.addChildNode(newTorus)
+      }
+    
+    
     @objc
     func handleTap(_ gestureRecognize: UIGestureRecognizer) {
+        addTorus()
         
         // retrieve the SCNView
-        let scnView = self.view as! SCNView
-        
-        // check what nodes are tapped
-        let p = gestureRecognize.location(in: scnView)
-        let hitResults = scnView.hitTest(p, options: [:])
-        // check that we clicked on at least one object
-        if hitResults.count > 0 {
-            // retrieved the first clicked object
-            let result = hitResults[0]
-            
-            // get its material
-            let material = result.node.geometry!.firstMaterial!
-            
-            // highlight it
-            SCNTransaction.begin()
-            SCNTransaction.animationDuration = 0.5
-            
-            // on completion - unhighlight
-            SCNTransaction.completionBlock = {
-                SCNTransaction.begin()
-                SCNTransaction.animationDuration = 0.5
-                
-                material.emission.contents = UIColor.black
-                
-                SCNTransaction.commit()
-            }
-            
-            material.emission.contents = UIColor.red
-            
-            SCNTransaction.commit()
-        }
+//        let scnView = self.view as! SCNView
+//
+//        // check what nodes are tapped
+//        let p = gestureRecognize.location(in: scnView)
+//        let hitResults = scnView.hitTest(p, options: [:])
+//        // check that we clicked on at least one object
+//        if hitResults.count > 0 {
+//            // retrieved the first clicked object
+//            let result = hitResults[0]
+//
+//            // get its material
+//            let material = result.node.geometry!.firstMaterial!
+//
+//            // highlight it
+//            SCNTransaction.begin()
+//            SCNTransaction.animationDuration = 0.5
+//
+//            // on completion - unhighlight
+//            SCNTransaction.completionBlock = {
+//                SCNTransaction.begin()
+//                SCNTransaction.animationDuration = 0.5
+//
+//                material.emission.contents = UIColor.black
+//
+//                SCNTransaction.commit()
+//            }
+//
+//            material.emission.contents = UIColor.red
+//
+//            SCNTransaction.commit()
+//        }
     }
     
     override var prefersStatusBarHidden: Bool {
